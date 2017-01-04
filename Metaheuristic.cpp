661d8d499d4793aab6b4a3aa12bf9bc31f27576e
Piotrekf3 <piotrek.fur@wp.pl>
2017-01-04 19:00:56 +0100

    zmiana w main

--- Metaheuristic.cpp
+++ Metaheuristic.cpp
@@ -1,27 +1,22 @@
 #include<iostream>
+#include<ctime>
 #include"structure.h"
 using namespace std;
 
 int main()
 {
-	Instance generator;
-	generator.load_from_file("../../Instances/Instancja1.txt");
-	//cout << generator.get_task(2).get_index() << endl;
-	generator.generate_solutions();
-	cout << "1 maszyna\n";
-	//generator.get_solution(2).show(1);
-	//cout << endl;
-	cout << "2 maszyna\n";
-	//generator.get_solution(2).show(2);
-	//cout << "koniec generowania rozw\n";
+	srand(time(NULL));
+	Instance instance;
+	instance.load_from_file("../../Instances/Instancja1.txt");
+	instance.generate_solutions();
+	time_t start = time(NULL);
+	//while (difftime(time(NULL), start) < 5)
 	for (int i = 0; i < 100; i++)
-	{	
-		generator.mutation();
-		generator.crossing();
-		
+	{
+		instance.mutation();
+		for (int j = 0; j < 100;j++)
+		instance.crossing();
 	}
-
-
 
 
 	cin.ignore();
